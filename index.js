afe9c96323034348d0b2a18fadca4215cbb47037
alistaircol <me@ac93.uk>
2023-03-18 15:02:12 +0000

    add testing workflow

--- index.js
+++ index.js
@@ -1,27 +1,18 @@
 const core = require('@actions/core');
-const fs = require('fs');
+const update_document = require("./lib/update_document");
 
 try {
-  const fileName = core.getInput('file');
-
-  if (!fs.existsSync(fileName)) {
-    core.setFailed(`File (${fileName}) does not exist`);
-    return;
-  }
-
+  const file = core.getInput('file');
   const delimiterOpen = core.getInput('delimiter-open');
   const delimiterClose = core.getInput('delimiter-close');
-
-  const file = fs.readFileSync(fileName, 'utf-8');
-
-  const startIndex = file.indexOf(delimiterOpen) + delimiterOpen.length;
-  const endIndex = file.indexOf(delimiterClose);
-
   const contents = `\n${core.getInput('content')}\n`;
 
-  const updatedReadme = file.slice(0, startIndex) + contents + file.slice(endIndex);
-
-  fs.writeFileSync(fileName, updatedReadme);
+  update_document(
+    file,
+    delimiterOpen,
+    delimiterClose,
+    contents
+  );
 } catch (e) {
   core.setFailed(e.message);
 }
